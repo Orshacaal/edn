9f1fbcb60c48ac0ef33e9bb19d38fd45d10fa1cc
Miller do E.S.B. <millerfcamara@gmail.com>
2025-06-17 21:58:50 -0300

    Add files via upload

---
+++ atividade-1-edn.py
@@ -0,0 +1,27 @@
+print("1-Programa de Saudação")
+print("Olá, mundo!")
+print()
+
+print("2- Calculadora de Soma")
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é igual a {soma}")
+print()
+print("3- Calculadora de Volume")
+comprimento = 12 # cm
+largura = 14 # cm
+altura = 20 # cm
+volume = comprimento * largura * altura
+print(f"O volume de uma caixa com {comprimento}cm de comprimento,")
+print(f"{largura}cm de largura e {altura}cm de altura é {volume}cm³")
+print()
+print("4- Calculadora de Preço Total")
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total:.2f}")
